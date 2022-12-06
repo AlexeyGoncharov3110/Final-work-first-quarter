d68d167aed36c5b36294d5950446c3b4a42a0bf8
Alexey <slow.net@mail.ru>
2022-12-06 18:00:21 +0300

    create an array of strings and ask the user to fill it

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -5,4 +5,15 @@ if (!isParsedN)
     Console.WriteLine("Ошибка!Повторите ввод");
     return;
 }
+string[] stringArray = StringArray(n);
+string[] StringArray(int n)
+{
+    string[] array = new string[n];
+    Console.WriteLine("Введите строки массива");
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
