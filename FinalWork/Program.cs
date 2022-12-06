2829cf4f1047d821e8f37bce11df62acdc6408f4
Alexey <slow.net@mail.ru>
2022-12-06 17:56:54 +0300

    enter the size of the array

---
+++ FinalWork/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите размер массива");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка!Повторите ввод");
+    return;
+}
+
